74249d86292a63c285d117f6d2a999b4105fe9ff
vnshanmukh <venkatanagashanmukh.chava@slu.edu>
2024-01-10 19:58:04 -0600

    coding challenges

--- src/codingChallenges/Calculator.java
+++ src/codingChallenges/Calculator.java
@@ -5,42 +5,37 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double number1 = input.nextDouble();
         while (true) {
-            System.out.print("Enter the first number: ");
-            double number1 = input.nextDouble();
-
-            System.out.println("Enter operation + , - , * , / ");
+            System.out.println("Enter operation + , - , * , / or stop ");
             String operation = input.next();
-
-            System.out.println("Enter the Second Number");
+            if (operation.equals("stop")){
+                System.out.println("total: " + number1 );
+                break;
+            }
+            System.out.println("Enter the Number");
             double number2 = input.nextDouble();
 
-            double res = 0;
+
             switch (operation){
                 case "+" :
-                    res = number1 + number2;
+                    number1 += number2;
                     break;
                 case "-" :
-                    res = number1 - number2;
+                    number1 -= number2;
                     break;
                 case "*" :
-                    res = number1*number2;
+                    number1 *=number2;
                     break;
                 case "/" :
                     try {
-                        res = number1/number2;
+                        number1 /= number2;
                     } catch (ArithmeticException e) {
                         throw new RuntimeException("Cannot divide by 0");
                     }
-
             }
-            System.out.println(res);
-            System.out.println("Do you want to continue?(yes/no)");
-            String continueOption = input.next().toLowerCase();
-            if (!continueOption.equals("yes")) {
-                break;
-            }
-
+            System.out.println("running total: "+ number1);
         }
 
     }
--- src/codingChallenges/excep.java
+++ src/codingChallenges/excep.java
@@ -4,6 +4,17 @@ public class excep {
 
     public static void main(String[] args) {
         int[] arr = new int[3];
-        System.out.println(arr[4]);
+        try {
+            System.out.println(arr[4]);
+        } catch (IndexOutOfBoundsException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] s = new String[9];
+        try {
+            System.out.println(s[0].length());
+        } catch (NullPointerException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
